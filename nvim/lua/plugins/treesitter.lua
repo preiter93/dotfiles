e23a4e3c942fbe609a4014679c8f6be2e4771601
Philipp Reiter <philipp.reiter@freiheit.com>
2024-02-15 15:58:32 +0100

    revert treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,12 @@
 -- Source:
 -- https://github.com/LazyVim/LazyVim/blob/9264c54ae96d1d56f029ad9b561326c7b991c53b/lua/lazyvim/plugins/treesitter.lua
 -- https://www.reddit.com/r/neovim/comments/15chdn3/help_me_understand_this_rtp_lazyvim_trick/
-local load_textobjects = false
 return {
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
-      init = function()
-        require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-        load_textobjects = true
-      end,
     },
     opts = {
       -- Add languages to be installed here that you want installed for treesitter
@@ -89,21 +82,6 @@ return {
     },
     config = function(_, opts)
       pcall(require('nvim-treesitter.install').update { with_sync = true })
-      require("nvim-treesitter.configs").setup(opts)
-
-      if load_textobjects then
-        if opts.textobjects then
-          for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-            if opts.textobjects[mod] and opts.textobjects[mod].enable then
-              local Loader = require("lazy.core.loader")
-              Loader.disabled_rtp_plugins["nvim-treesitter-textobjects"] = nil
-              local plugin = require("lazy.core.config").plugins["nvim-treesitter-textobjects"]
-              require("lazy.core.loader").source_runtime(plugin.dir, "plugin")
-              break
-            end
-          end
-        end
-      end
     end,
   },
 }
