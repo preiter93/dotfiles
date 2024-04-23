ec2d6b89d7e6c9300b07069e4bb473b4255c85ab
Philipp Reiter <philipp.reiter@freiheit.com>
2024-04-23 11:56:35 +0200

    Update nvim config

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,5 +1,6 @@
 return {
   'folke/flash.nvim',
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     -- Setup
     require('flash').setup({
@@ -11,6 +12,7 @@ return {
     })
     -- Keymappings
     vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
-    vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
+    vim.keymap.set({ "n", "x", "o" }, "<leader>ft", function() require("flash").treesitter() end,
+      { desc = "[F]lash [T]reesitter" })
   end
 }
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,5 +1,6 @@
 return {
   -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
+  event = "VeryLazy",
   opts = {},
 }
