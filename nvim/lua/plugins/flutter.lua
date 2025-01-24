a7030cfcbe4b10fdaf1f5561db8fa6b4f86703f5
Philipp Reiter <p.reiter93@gmail.con>
2025-01-24 07:38:20 +0100

    nvim: Add mappings for flutter tools

--- nvim/lua/plugins/flutter.lua
+++ nvim/lua/plugins/flutter.lua
@@ -1,14 +1,31 @@
 return {
-    'akinsho/flutter-tools.nvim',
+    'nvim-flutter/flutter-tools.nvim',
     dependencies = { 'mason-lspconfig.nvim', 'nvim-lua/plenary.nvim', 'stevearc/dressing.nvim' },
-    opts = {
-        lsp = {
-            on_attach = function(client, bufnr)
-                require('utils').on_attach(client, bufnr, true)
-            end,
-        },
-        settings = {
-            enableSnippets = false,
+    config = function()
+        require("flutter-tools").setup {
+            lsp = {
+                on_attach = function(client, bufnr)
+                    require('utils').on_attach(client, bufnr, true)
+
+                    if vim.bo[bufnr].filetype == "dart" then
+                        local opts = { noremap = true, silent = true }
+
+                        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fo', ':FlutterOutlineToggle<CR>', opts)
+                        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fs', ':FlutterRun<CR>', opts)
+                        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fr', ':FlutterReload<CR>', opts)
+                        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fa', ':FlutterAttach<CR>', opts)
+                        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fd', ':FlutterDetach<CR>', opts)
+                    end
+                end,
+            },
+            settings = {
+                enableSnippets = false,
+            },
         }
-    }
+        require('flutter-tools').setup_project({
+            name = 'Web',
+            device = 'chrome',
+            web_port = "8080",
+        })
+    end
 }
