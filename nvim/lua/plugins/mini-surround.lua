bda8e6b1130ffe1a45093c68be6addd3b0f2e4a5
Philipp Reiter <p.reiter93@gmail.con>
2025-09-21 10:22:34 +0200

    nvim: update snippets

--- nvim/lua/plugins/mini-surround.lua
+++ nvim/lua/plugins/mini-surround.lua
@@ -5,12 +5,10 @@ return {
     local MiniSurround = require('mini.surround')
     MiniSurround.setup({
       mappings = {
-        add = "gsa",       -- Add surrounding in Normal and Visual modes
-        delete = "gsd",    -- Delete surrounding
-        find = "gsf",      -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr",   -- Replace surrounding
+        add = "gs",      -- Add surrounding in Normal and Visual modes
+        delete = "gsd",  -- Delete surrounding
+        find = "gsf",    -- Find surrounding (to the right)
+        replace = "gsr", -- Replace surrounding
       },
     })
   end
